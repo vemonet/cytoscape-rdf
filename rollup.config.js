d3d0364631e965bdde4a09a97c2c3f94cd554d81
Vincent Emonet <vincent.emonet@gmail.com>
2023-03-01 09:57:22 +0100

    fix tsconfig

--- rollup.config.js
+++ rollup.config.js
@@ -1,61 +1,3 @@
-// import transformTaggedTemplate from 'rollup-plugin-transform-tagged-template';
-// import typescript from '@rollup/plugin-typescript';
-// import resolve from '@rollup/plugin-node-resolve';
-// import cleaner from 'rollup-plugin-cleaner';
-// import copy from 'rollup-plugin-copy';
-// import serve from 'rollup-plugin-serve';
-// import { terser } from 'rollup-plugin-terser';
-
-// export default {
-//   input: 'src/index.ts',
-//   output: {
-//     file: 'dist/bundle.js',
-//     name: 'bundle',
-//     format: 'umd',
-//     sourcemap: true
-//   },
-//   plugins: [
-//     transformTaggedTemplate({
-//       tagsToProcess: ['html','css'],
-//       parserOptions: {
-//         sourceType: "module",
-//         plugins: [
-//             "typescript",
-//             [
-//                 "decorators",
-//                 { decoratorsBeforeExport: true }
-//             ]
-//         ]
-//       },
-//       transformer(data) {
-//           data = data.replace(/\s([{}()>~+=^$:!;])\s/gm, '$1');
-//           data = data.replace(/([",[]])\s+/gm, '$1');
-//           data = data.replace(/\s{2,}/gm, ' ');
-//           return data.trim();
-//       }
-//     }),
-//     typescript(),
-//     resolve(),
-//     cleaner({
-//       targets: [
-//       'dist'
-//       ]
-//     }),
-//     copy({
-//       targets: [
-//         { src: 'index.html', dest: 'dist' },
-//       ]
-//     }),
-//     serve({
-//       open: true,
-//       contentBase: 'dist'
-//     }),
-//     terser(),
-//   ]
-// };
-
-
-
 // https://github.com/microsoft/fast/blob/master/packages/web-components/fast-element/rollup.config.js
 import filesize from "rollup-plugin-filesize";
 import { terser } from "rollup-plugin-terser";
@@ -73,8 +15,8 @@ const plugins = [
         },
     }),
     // TODO: resolves imports, but fail due to N3.js https://github.com/rdfjs/N3.js/issues/257
-    // nodeResolve({ preferBuiltins: false }),
-    // commonjs(),
+    commonjs(),
+    nodeResolve({ preferBuiltins: false }),
     // nodeGlobals(),
     // nodeBuiltins(),
     filesize({
